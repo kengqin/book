ce7d383e7f09bbd27dfee1ebf58c27b7ad2fe4c8
chengxin <1477906555@qq.com>
2026-07-16 15:31:52 +0800

    fix: make IDE plugin builds work on Windows CI

--- plugins/intellij/build.gradle.kts
+++ plugins/intellij/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 group = "com.kengqin.novellibrary"
 version = "0.4.0"
 
-repositories { mavenCentral(); intellijPlatform { defaultRepositories() } }
 dependencies { intellijPlatform { intellijIdeaCommunity("2024.1") } }
 
 intellijPlatform { pluginConfiguration { ideaVersion { sinceBuild = "241" }; changeNotes = "Novel Library IDE reader and local Bridge sync." } }
--- plugins/intellij/settings.gradle.kts
+++ plugins/intellij/settings.gradle.kts
@@ -1,5 +1,11 @@
 import org.gradle.api.initialization.resolve.RepositoriesMode
 
 pluginManagement { repositories { gradlePluginPortal(); mavenCentral() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { mavenCentral() } }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        intellijPlatform { defaultRepositories() }
+    }
+}
 rootProject.name = "novel-library-intellij"
